88bb7127770ffb4270322b925269521719eba71d
shellew <nishioka2013_1124@yahoo.co.jp>
2023-07-15 16:47:10 +0900

    タスクに優先度の項目を追加

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 tasks = []
 
+class Task:
+    def __init__(self, title, description, due_date, status, priority):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.status = status
+        self.priority = priority
+
 @app.route('/')
 def index():
     return render_template('index.html', tasks=tasks)
@@ -13,12 +21,9 @@ def create():
     title = request.form.get('title')
     description = request.form.get('description')
     due_date = request.form.get('due_date')
-    task = {
-        'title': title,
-        'description': description,
-        'due_date': due_date,
-        'status': '未着手'
-    }
+    status = '未着手'
+    priority = request.form.get('priority')
+    task = Task(title, description, due_date, status, priority)
     tasks.append(task)
     return redirect('/')
 
@@ -26,10 +31,13 @@ def create():
 def edit(task_id):
     if request.method == 'POST':
         new_status = request.form.get('status')
-        tasks[task_id]['status'] = new_status
+        priority = request.form.get('priority')
+        tasks[task_id].status = new_status
+        tasks[task_id].priority = priority
         return redirect('/')
     else:
-        return render_template('edit.html', task=tasks[task_id], task_id=task_id)
+        task = tasks[task_id]
+        return render_template('edit.html', task=task, task_id=task_id)
 
 @app.route('/delete/<int:task_id>', methods=['POST'])
 def delete(task_id):
